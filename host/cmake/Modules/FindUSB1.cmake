498b81541b05736b6bd76aa713656992de4ced83
Tanguy Thoraval <tanguy.thoraval@nav-timing.safrangroup.com>
2026-08-05 11:22:27 +0200

    [FIX] Link to libusb for windows

--- host/cmake/Modules/FindUSB1.cmake
+++ host/cmake/Modules/FindUSB1.cmake
@@ -6,32 +6,50 @@
 include(FindPkgConfig)
 PKG_CHECK_MODULES(PC_LIBUSB QUIET libusb-1.0)
 
-find_path(LIBUSB_INCLUDE_DIRS
-    NAMES libusb.h
-    HINTS $ENV{LIBUSB_DIR}/include $ENV{LIBUSB_DIR}/include/libusb-1.0
-    ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDEDIR}/libusb-1.0
-    PATHS /usr/local/include/libusb-1.0 /usr/local/include
-    /usr/include/libusb-1.0 /usr/include
-    /opt/local/include/libusb-1.0
 
-    #non-conforming naming convention,
-    #backwards compatible with old script
-    ${LIBUSB_INCLUDE_DIR}
-)
+if (MSVC)
+    find_path(LIBUSB_INCLUDE_DIRS
+        NAMES libusb.h
+        HINTS "C:/Program Files/libusb/include"
+        #non-conforming naming convention,
+        #backwards compatible with old script
+        ${LIBUSB_INCLUDE_DIR}
+    )
 
-#standard library name for libusb-1.0
-set(libusb1_library_names usb-1.0 libusb-1.0)
+    set(libusb1_library_names libusb-1.0)
+    find_library(LIBUSB_LIBRARIES
+        NAMES ${libusb1_library_names}
+        HINTS "C:/Program Files/libusb/VS2022/MS64/static"
+        ${LIBUSB_LIB_DIR}
+    )
+else()
+    find_path(LIBUSB_INCLUDE_DIRS
+        NAMES libusb.h
+        HINTS $ENV{LIBUSB_DIR}/include $ENV{LIBUSB_DIR}/include/libusb-1.0
+        ${PC_LIBUSB_INCLUDEDIR} ${PC_LIBUSB_INCLUDEDIR}/libusb-1.0
+        PATHS /usr/local/include/libusb-1.0 /usr/local/include
+        /usr/include/libusb-1.0 /usr/include
+        /opt/local/include/libusb-1.0
 
-#libusb-1.0 compatible library on freebsd
-if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD" OR CMAKE_SYSTEM_NAME STREQUAL "kFreeBSD")
-    list(APPEND libusb1_library_names usb)
-endif()
+        #non-conforming naming convention,
+        #backwards compatible with old script
+        ${LIBUSB_INCLUDE_DIR}
+    )
 
-find_library(LIBUSB_LIBRARIES
-    NAMES ${libusb1_library_names}
-    HINTS $ENV{LIBUSB_DIR}/lib ${PC_LIBUSB_LIBDIR}
-    PATHS /usr/local/lib /usr/lib /opt/local/lib
-)
+    #standard library name for libusb-1.0
+    set(libusb1_library_names usb-1.0 libusb-1.0)
+
+    #libusb-1.0 compatible library on freebsd
+    if(CMAKE_SYSTEM_NAME STREQUAL "FreeBSD" OR CMAKE_SYSTEM_NAME STREQUAL "kFreeBSD")
+        list(APPEND libusb1_library_names usb)
+    endif()
+
+    find_library(LIBUSB_LIBRARIES
+        NAMES ${libusb1_library_names}
+        HINTS $ENV{LIBUSB_DIR}/lib ${PC_LIBUSB_LIBDIR}
+        PATHS /usr/local/lib /usr/lib /opt/local/lib
+    )
+endif(MSVC)
 
 enable_language(C) #needs C support for check below
 include(CheckFunctionExists)
